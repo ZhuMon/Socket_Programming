f80956139ec3e39e3ac4a7e60e526ae844fa735a
YuXiang <richardlin0212@gmail.com>
2019-03-26 11:32:42 +0800

    Fisish TCP Add print_time() & Separate to 2 part to handle big file transfer

--- lab1_file_transfer.c
+++ lab1_file_transfer.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <netinet/in.h>
 #include <netdb.h>
 
@@ -13,7 +14,15 @@ char *file_name; //store file name
 struct hostent *server; //store server ip
 FILE *file;   //the file want to transfer
 struct stat f_state; //store state of the file
+struct timeval my_t; //record arrival time in usec & sec
+struct tm *p; //record time in year, month ...
 
+void print_time(int percent){
+    gettimeofday(&my_t, NULL); //get sec & usec from 1970/1/1 0:00:00
+    p = localtime(&my_t.tv_sec); //turn second to year, month, day, ...
+    printf("%3d%% %02d/%02d/%02d %02d:%02d:%02d.%06d\n", percent, (1900+p->tm_year), (1+p->tm_mon), p->tm_mday, p->tm_hour, p->tm_min, p->tm_sec, my_t.tv_usec);
+
+}
 
 void error(const char *msg){
     perror(msg);
@@ -23,7 +32,6 @@ void error(const char *msg){
 void tcp_s(){
     int sockfd, newsockfd;
     socklen_t clilen;
-    char buffer[256];   //to store received data
     struct sockaddr_in serv_addr, cli_addr;
     int n;
 
@@ -49,6 +57,8 @@ void tcp_s(){
     if (newsockfd < 0)
         error("ERROR on accept");
 
+    char buffer[256];   //to store data of first packet
+    
     bzero(buffer, 256);
     n = read(newsockfd, buffer, 256);
     
@@ -94,6 +104,34 @@ void tcp_s(){
 
     printf("File size: %d\n", file_size_i);
 
+    int fp_size = (int)(file_size_i/20.0+0.5); //size of packet is file_size/20 四捨五入
+    int p_size; //packet size
+    
+    //if 5% file >= 256 bytes, after transfer, file will crash. So, let size of packet limited to 256 bytes
+    if(fp_size >= 256){
+        p_size = 256;
+    } else {
+        p_size = fp_size; //each packet size = 5% file size
+    }
+    
+    
+    char p_buffer[p_size]; //buffer for each packet
+    
+    int fp_num; //packet number = 20(the last packet will be big)
+               //             or 21(the last packet will be small) 
+    if(fp_size == (int) file_size_i/20){
+        fp_num = 21;
+    } else if(fp_size == (int) file_size_i/20 + 1){
+        fp_num = 20;
+    } else {
+        error("Error, packet_size error");
+    }
+
+    if(file_size_i % 20 == 0){ //如果20個packet可以平分完file，就只使用20個packet
+        fp_num = 20;
+    }
+
+    /*
     ////parse third line in buffer
     char p_num_c[256]; //store packet number in character
     int p_num_i; //store packet number in integer
@@ -113,30 +151,88 @@ void tcp_s(){
     p_num_i = atoi(p_num_c);
 
     printf("packet number: %d\n", p_num_i);
+    */
 
-    for(int i = 0; i < p_num_i; i++){
-        /*newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-    
-        if (newsockfd < 0)
-            error("ERROR on accept");
-        */
-        bzero(buffer, 256);
-        if(i == p_num_i-1){
-            n = read(newsockfd, buffer, file_size_i%256);
-            fwrite(buffer, 1, file_size_i%256, outfile);
-        } else {
-            n = read(newsockfd, buffer, 256);
-            fwrite(buffer, 1, 256, outfile);
+    if(p_size == fp_size){    // if 5% file <= 256 bytes (only 20 or 21 packets)
+        for(int i = 0; i < fp_num; i++){
+            bzero(p_buffer, p_size);
+
+            if(i == fp_num-1){
+                n = read(newsockfd, p_buffer, file_size_i % p_size);
+                fwrite(p_buffer, 1, file_size_i % p_size, outfile);
+                print_time(100); //print 100%
+            } else {
+                n = read(newsockfd, p_buffer, p_size);
+                fwrite(p_buffer, 1, p_size, outfile);
+                if(i != 19){
+                    print_time(5*(i+1));
+                }
+            }
         }
-    }
+    } else { //each packet team has more than 1 packet
+        int tp_num; // X packets/team. Record packet num of each team (except the last) 
+        int tp_num_l; // X packets/team. Record packet number of the last team
+        
+        //分析每個team有多少個packets
+        //無條件進位
+        if(fp_size % 256 != 0){
+            tp_num = fp_size/256 + 1;
+        } else {
+            tp_num = fp_size/256;
+        }
 
+        //最後一個team的packet 個數
+        if(file_size_i % 20 == 0){ //若20個team可以平分file，最後一個team的packet個數就會與其他的一樣
+            tp_num_l = tp_num;
+        }else{
+            if(file_size_i % fp_size % 256 != 0){
+                tp_num_l = file_size_i % fp_size /256 + 1;
+            } else {
+                tp_num_l = file_size_i % fp_size /256;
+            }
+        }
+
+         //每個i代表1個team
+        for(int i = 0; i < fp_num; i++){
+            bzero(p_buffer, 256);
+
+            if(i == fp_num -1 && file_size_i % 20 != 0){ //the last team whose packets different than others
+                for(int j = 0; j < tp_num_l; j++){
+                    if(j == tp_num_l -1){
+                        n = read(newsockfd, p_buffer, file_size_i%fp_size%256);
+                        fwrite(p_buffer, 1, file_size_i%fp_size%256, outfile);
+                        print_time(100);
+                     } else {
+                        n = read(newsockfd, p_buffer,256);
+                        fwrite(p_buffer, 1, 256, outfile);
+                     }
+                 }
+            } else { //the other team
+                //每個j代表team中的1個packet
+                for(int j = 0; j < tp_num; j++){
+                    if(j == tp_num - 1){  //the last packet
+                        n = read(newsockfd, p_buffer, fp_size%256);
+                        fwrite(p_buffer, 1, fp_size%256, outfile);
+                        if(i != 19){
+                            print_time((i+1)*5);
+                        }
+                    } else{
+                        //每個k代表buffer的一個byte
+                        n = read(newsockfd, p_buffer, 256);
+                        fwrite(p_buffer, 1, 256, outfile);
+                    }
+                }
+            }
+        }
+
+    }
 
     //fwrite(buffer, 1, sizeof(buffer), outfile);
 
     fclose(outfile);
 
     //printf("Here is the message: \n%s\n", buffer);
-    n = write(newsockfd, "server : I got your message", 27);
+    n = write(newsockfd, "Finish transfer", 15);
 
     if (n < 0) error("ERROR writing to socket");
 
@@ -152,7 +248,6 @@ void tcp_c(){
     int sockfd, n;
     struct sockaddr_in serv_addr;
 
-    //char buffer[25600];
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (sockfd < 0) 
@@ -175,57 +270,169 @@ void tcp_c(){
 
     strcat(f_buffer, "\n");   
 
-    char file_size[256];
-    sprintf(file_size, "%lld", f_state.st_size);  //change file_size from int to char
-    strcat(f_buffer, file_size); //store file size in second line
+    char file_size_c[256];
+    sprintf(file_size_c, "%lld", f_state.st_size);  //change file size from int to char
+    strcat(f_buffer, file_size_c); //store file size in second line
     strcat(f_buffer, "\n");
 
-    char p_num[256]; //the number of packet that the file will spilt into 
-    sprintf(p_num, "%lld", f_state.st_size/256 +1); //change packet number from int to char
+    
+    int fp_size = f_state.st_size/20.0 + 0.5; //5% of packets size is file size/20 四捨五入
+    int p_size; //packet size;
+    
+    //if 5% file >= 256 bytes, after transfer, file will crash. So, let size of packet limited to 256 bytes
+    if(fp_size >= 256){  
+        p_size = 256;
+    } else {
+        p_size = fp_size; //each packet size = 5% file size
+    }
+
+    char p_buffer[p_size]; //buffer for each packet
+
+    
+    // 5% packet number = 20(the last packet will be big)
+    //                 or 21(the last packet will be small) 
+    // in order to make each packet team equal to 5% file
+    // a packet team consist of 
+    //     many 256B_packets & (one <256B_packet or none)
+    // no team:
+    //   681 = 20*34 + 1*1      -> 21 packets
+    //   699 = 19*35 + 1*34     -> 20 packets
+    // team:
+    //   20519 = 19*(4*256+1*2)+1*(4*256+1*1) -> 20 packet teams -> 100 packets
+
+    int fp_num;
+    if(fp_size == f_state.st_size/20){
+        fp_num = 21;
+    } else if(fp_size == f_state.st_size/20 + 1){
+        fp_num = 20;
+    }
+    if(f_state.st_size % 20 == 0){ //如果20個packet可以平分完file，就只使用20個packet
+        fp_num = 20;
+    }
+
+    /*
+    char p_num[256]; //5% of the number of packet that the file will spilt into 
+    sprintf(p_num, "%lld", f_state.st_size/p_num +1); //change packet number from int to char
     strcat(f_buffer, p_num);
     printf("f_buffer: %s\n", f_buffer);
     strcat(f_buffer, "\n");
+    */
 
     n = write(sockfd, f_buffer, 256);
     if (n < 0) 
          error("ERROR writing to socket. (first packet)");
 
-    //buffer store the transfered file
+    //read file
     char buffer[f_state.st_size+1]; //make buffer equal to size of file
     bzero(buffer, f_state.st_size+1); //clean buffer
     fread(buffer, f_state.st_size, 1, file); //read file to buffer
     
-    char s_buffer[256]; //small buffer for each packet
-    for(int i = 0; i < f_state.st_size/256 + 1; i++){
-        bzero(s_buffer, 256);  
 
-        if(i == f_state.st_size/256){ //the last packet or file size < 256
-            for(int j = 0; j < f_state.st_size%256;j++){
-                s_buffer[j] = buffer[i*256+j];
+
+    //pass packets split from file
+    if(p_size == fp_size){    // if 5% file <= 256 bytes (only 20 or 21 packets)
+        for(int i = 0; i < fp_num ; i++){
+            bzero(p_buffer, p_size);  
+
+            if(i == fp_num-1){ //the last packet
+                //若20個packet可以平分file，最後一個packet還是使用 p_size
+                if(fp_size == f_state.st_size/20){ 
+                    for(int j = 0; j < p_size; j++){  
+                        p_buffer[j] = buffer[i*p_size+j];
+                    }
+                    n = write(sockfd,p_buffer,p_size);
+                    break;
+                } 
+
+                //最後一個packet 不會是滿的，所以j只到f_state.st_size%p_size
+                for(int j = 0; j < f_state.st_size%p_size; j++){
+                    p_buffer[j] = buffer[i*p_size+j];
+                }
+                n = write(sockfd,p_buffer, f_state.st_size%p_size);
+            } else { 
+                //spilt file into 19 or 20 packets
+                //and transfer them
+                for(int j = 0; j < p_size; j++){  
+                    p_buffer[j] = buffer[i*p_size+j];
+                }
+                n = write(sockfd,p_buffer,p_size);
             }
-            n = write(sockfd,s_buffer, f_state.st_size%256);
+
+            if (n < 0) 
+                 error("ERROR writing to socket. (file packet)");
+        }
+    } else { //each packet team has more than 1 packet
+        int tp_num; // X packets/team. Record packet num of each team (except the last) 
+        int tp_num_l; // X packets/team. Record packet number of the last team
+        
+        //分析每個team有多少個packets
+        //無條件進位
+        if(fp_size % 256 != 0){
+            tp_num = fp_size/256 + 1;
         } else {
-            for(int j = 0; j < 256; j++){  
-                s_buffer[j] = buffer[i*256+j];
-            }
-            n = write(sockfd,s_buffer,256);
+            tp_num = fp_size/256;
         }
 
-        if (n < 0) 
-             error("ERROR writing to socket. (file packet)");
+        //最後一個team的packet 個數
+        if(f_state.st_size % 20 == 0){ //若20個team可以平分file，最後一個team的packet個數就會與其他的一樣
+            tp_num_l = tp_num;
+        }else{
+            if(f_state.st_size % fp_size % 256 != 0){
+                tp_num_l = f_state.st_size % fp_size /256 + 1;
+            } else {
+                tp_num_l = f_state.st_size % fp_size /256;
+            }
+        }
+
+        //每個i代表1個team
+        for(int i = 0; i < fp_num; i++){
+            bzero(p_buffer, 256);
+
+            if(i == fp_num -1 && f_state.st_size % 20 != 0){ //the last team whose packets different than others
+                 for(int j = 0; j < tp_num_l; j++){
+                     if(j == tp_num_l -1){
+                         for(int k = 0; k < f_state.st_size%fp_size%256; k++){
+                             p_buffer[k] = buffer[i*fp_size+j*256+k];
+                         }
+                         n = write(sockfd, p_buffer, f_state.st_size%fp_size%256);
+
+                     } else {
+                        for(int k = 0; k < 256; k++){
+                            p_buffer[k] = buffer[i*fp_size+j*256+k];
+                        }
+                        n = write(sockfd, p_buffer, 256);
+                     }
+                 }
+            } else { //the other team
+                //每個j代表team中的1個packet
+                for(int j = 0; j < tp_num; j++){
+                    if(j == tp_num - 1){  //the last packet
+                        for(int k = 0; k < fp_size%256;k++){
+                            p_buffer[k] = buffer[i*fp_size + j*256 + k];
+                        }
+                        n = write(sockfd, p_buffer, fp_size%256);
+                    } else{
+                        //每個k代表buffer的一個byte
+                        for(int k = 0; k < 256; k++){  
+                            p_buffer[k] = buffer[i*fp_size+j*256+k];
+                        }
+                        n = write(sockfd,p_buffer,256);
+                    }
+                }
+            }
+        }
     }
-    
     /*
     n = write(sockfd,buffer,strlen(buffer));
 
     if (n < 0) 
          error("ERROR writing to socket");
     */
-    bzero(s_buffer,256);
-    n = read(sockfd,s_buffer,256);
+    bzero(f_buffer,256);
+    n = read(sockfd,f_buffer,15);
     if (n < 0) 
          error("ERROR reading from socket");
-    printf("%s\n",s_buffer);
+    printf("%s\n",f_buffer);
     close(sockfd);
     return;
 
